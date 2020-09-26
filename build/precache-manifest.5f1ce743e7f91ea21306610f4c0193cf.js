3c6f03cc9e74683b9bdd2b8dad013ef31b84eaab
Jayesh Singh <jayeshsingh001@gmail.com>
2020-09-26 22:08:52 +0530

    complete and deploy

---
+++ build/precache-manifest.5f1ce743e7f91ea21306610f4c0193cf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e16e6f04bd33f8a0549679dd8371122d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed85cbea89390cbd83c7",
+    "url": "/static/js/2.8c98b8e3.chunk.js"
+  },
+  {
+    "revision": "ec41a04f4c0ce9a2fa00",
+    "url": "/static/js/main.9d830de7.chunk.js"
+  },
+  {
+    "revision": "9c81ea09b8c333c56883",
+    "url": "/static/js/runtime-main.5537e3ce.js"
+  }
+]);
